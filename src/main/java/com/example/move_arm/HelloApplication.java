fb9936d823db468ff77822ad413f88ca691cf1a1
alexeyavdeenok <alexavdeenok@gmail.com>
2025-10-10 19:51:07 +0700

    1

--- src/main/java/com/example/move_arm/HelloApplication.java
+++ src/main/java/com/example/move_arm/HelloApplication.java
@@ -1,19 +1,28 @@
 package com.example.move_arm;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        // Загружаем наш FXML файл
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("start-window.fxml"));
+        Parent root = fxmlLoader.load();
+        
+        Scene scene = new Scene(root, 800, 600);
+        
+        stage.setTitle("Move Arm - Управление рукой");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
