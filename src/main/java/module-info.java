541a05cbbe8e6e62670927cb72ee46b95957404c
Ivan <ivan.stepanov.05@list.ru>
2025-10-10 17:31:45 +0700

    hello application

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.move_arm {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.move_arm to javafx.fxml;
+    exports com.example.move_arm;
+}
